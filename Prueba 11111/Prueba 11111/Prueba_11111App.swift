d00d2f78ef1856ee2a0d8ee44e68e5023835cdd9
d077 DIT UPM <d077@l149.lab.dit.upm.es>
2024-11-25 16:24:06 +0100

    Hello world

---
+++ Prueba 11111/Prueba 11111/Prueba_11111App.swift
@@ -0,0 +1,17 @@
+//
+//  Prueba_11111App.swift
+//  Prueba 11111
+//
+//  Created by d077 DIT UPM on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Prueba_11111App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
